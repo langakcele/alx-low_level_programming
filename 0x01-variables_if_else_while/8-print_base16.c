7fb11648d010ddbb9d2ddeb6512fd524744ccccd
langakcele <21810673@mygibs.co.za>
2023-07-16 14:52:59 -0700

    8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number;
+	char letter;
+
+	for (number = 0; number <= 9; number++)
+	{
+		putchar(number + '0');
+	}
+
+	for (letter = 'a'; letter <= 'f'; letter++)
+	{
+		putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}

5f0fa92b1a18199bcdca05db27556e48d8b46c3b
langakcele <21810673@mygibs.co.za>
2023-07-24 14:34:25 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,26 +7,26 @@
  */
 void puts_half(char *str)
 {
-    int j = 0;
-    int k;
+	int j = 0;
+	int k;
 
-    while (str[j] != '\0')
-    {
-        j++;
-    }
+	while (str[j] != '\0')
+	{
+		j++;
+	}
 
-    if (j % 2 == 1)
-    {
-        k = (j - 1) / 2;
-    }
-    else
-    {
-        k = j / 2;
-    }
+	if (j % 2 == 1)
+	{
+		k = (j - 1) / 2;
+	}
+	else
+	{
+		k = j / 2;
+	}
 
-    for (; k < j; k++)
-    {
-        _putchar(str[k]);
-    }
-    _putchar('\n');
+	for (; k < j; k++)
+	{
+		_putchar(str[k]);
+	}
+	_putchar('\n');
 }
